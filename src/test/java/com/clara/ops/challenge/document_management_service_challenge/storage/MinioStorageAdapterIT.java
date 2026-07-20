154ef4be4b32e58449fcb3db1d1f6cf670abf20c
Fernando Castelan <fercc1097@gmail.com>
2026-07-20 17:41:46 -0600

    test: share singleton Testcontainers across IT suite to remove resource contention

--- src/test/java/com/clara/ops/challenge/document_management_service_challenge/storage/MinioStorageAdapterIT.java
+++ src/test/java/com/clara/ops/challenge/document_management_service_challenge/storage/MinioStorageAdapterIT.java
@@ -2,45 +2,15 @@ package com.clara.ops.challenge.document_management_service_challenge.storage;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import com.clara.ops.challenge.document_management_service_challenge.config.MinioProperties;
-import io.minio.*;
+import com.clara.ops.challenge.document_management_service_challenge.support.IntegrationTest;
 import java.net.URI;
 import java.net.http.*;
 import org.junit.jupiter.api.*;
-import org.testcontainers.containers.MinIOContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
+import org.springframework.beans.factory.annotation.Autowired;
 
-@Testcontainers
-class MinioStorageAdapterIT {
+class MinioStorageAdapterIT extends IntegrationTest {
 
-  @Container
-  static MinIOContainer MINIO = new MinIOContainer("minio/minio:RELEASE.2024-01-16T16-07-38Z");
-
-  static MinioProperties props;
-  static MinioStorageAdapter adapter;
-
-  @BeforeAll
-  static void setup() throws Exception {
-    String ep = MINIO.getS3URL();
-    props =
-        new MinioProperties(
-            ep,
-            ep,
-            MINIO.getUserName(),
-            MINIO.getPassword(),
-            "document-bucket",
-            3600,
-            3600,
-            524288000L);
-    MinioClient client =
-        MinioClient.builder()
-            .endpoint(ep)
-            .credentials(MINIO.getUserName(), MINIO.getPassword())
-            .build();
-    client.makeBucket(MakeBucketArgs.builder().bucket("document-bucket").build());
-    adapter = new MinioStorageAdapter(client, client, props);
-  }
+  @Autowired MinioStorageAdapter adapter;
 
   @Test
   void presignedPutRoundTripThenStat() throws Exception {
